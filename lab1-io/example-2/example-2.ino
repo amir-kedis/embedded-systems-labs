29d7518c7ff78bb61f6ebb23c9c8b5debc8a9a80
Abdulrahman-Mostafa10 <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2025-04-26 14:04:15 +0300

    fix: fixing the bug in the initalization of LEDs rather than the switches

--- lab1-io/example-2/example-2.ino
+++ lab1-io/example-2/example-2.ino
@@ -6,39 +6,44 @@
 #ifndef PIN_MACROS_H
 #define PIN_MACROS_H
 
-#define PIN_SET(REG, PIN)       REG |= (1 << PIN)
-#define PIN_UNSET(REG, PIN)     REG &= ~(1 << PIN)
-#define PIN_TOGGLE(REG, PIN)    REG ^= (1 << PIN)
-#define PIN_READ(REG, PIN)      ((REG & (1 << PIN)) >> PIN)
+#define PIN_SET(REG, PIN) REG |= (1 << PIN)
+#define PIN_UNSET(REG, PIN) REG &= ~(1 << PIN)
+#define PIN_TOGGLE(REG, PIN) REG ^= (1 << PIN)
+#define PIN_READ(REG, PIN) ((REG & (1 << PIN)) >> PIN)
 
 #endif
 
-
-
-int main(void) {
+int main(void)
+{
   PIN_UNSET(DDRB, PB0);
   PIN_UNSET(DDRB, PB1);
 
   PIN_SET(DDRC, PC0);
   PIN_SET(DDRC, PC1);
 
-  PIN_UNSET(PORTB, PC0);
-  PIN_UNSET(PORTB, PC1);
+  PIN_UNSET(PORTC, PC0);
+  PIN_UNSET(PORTC, PC1);
 
-  while (1) {
+  while (1)
+  {
 
-    if (PIN_READ(PINB, PB0)) {
+    if (PIN_READ(PINB, PB0))
+    {
       PIN_SET(PORTC, PC0);
-    } else {
+    }
+    else
+    {
       PIN_UNSET(PORTC, PC0);
     }
 
-    if (PIN_READ(PINB, PB1)) {
+    if (PIN_READ(PINB, PB1))
+    {
       PIN_SET(PORTC, PC1);
-    } else {
+    }
+    else
+    {
       PIN_UNSET(PORTC, PC1);
     }
-
   }
 
   return 0;
